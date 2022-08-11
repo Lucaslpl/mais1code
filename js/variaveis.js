63a15bdb7525120df9176fac13655026f7720e6f
Lucas Luna <lucasbereshit@gmail.com>
2022-08-11 20:35:30 -0300

    Variaveis e tipos de dados

--- js/variaveis.js
+++ js/variaveis.js
@@ -46,3 +46,41 @@
 // console.log(nome)
 
 // meuNome()
+
+
+
+//VARIAVEIS E TIPOS DE DADOS
+
+const nome = "Lucas"
+// string
+
+const numero = 30
+// number
+
+const produtoEnviado = true
+const produtoNaoEnviado = false
+// boolean
+
+const elemento = document.querySelector('h1')
+// elementos
+
+const frutas = ["banana", "maça", "perâ"] //arrays
+
+// console.log(frutas[1])
+
+// frutas.map(fruta => console.log(fruta)) // array
+
+//objeto 
+const pessoa = {
+    nome: "Lucas",
+    idade:27,
+    endereco:{
+    bairro:"Jardim Castelo",
+    cidade:"Ferraz de Vasconcelos"
+    }
+    }
+    
+    console.log(pessoa.bairro) // object
+
+
+
