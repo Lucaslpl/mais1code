a455e5a1ebf1c1f5f93aaf4f8210ae69070b6778
Lucas Luna <lucasbereshit@gmail.com>
2022-08-11 19:59:04 -0300

    var, let e const

--- js/variaveis.js
+++ js/variaveis.js
@@ -1,3 +1,48 @@
-const nome = 'Lucas'
+// const nome = 'Lucas'
 
-console.log('meu nome é ', nome)
+// console.log('meu nome é ', nome)
+
+
+// variavel declarada com var
+// var nome = "Luciano"
+
+// function meuNome(){
+// 	var nome = "João"
+// 	console.log(nome)
+// }
+
+// var nome = 20
+
+// console.log(nome)
+
+// meuNome()
+
+// VARIAVEL COM LET
+// let nome = "Luciano"
+
+// function meuNome(){
+// 	var nome = "João"
+// 	console.log(nome)
+// }
+
+// nome = 20
+
+// console.log(nome)
+
+// meuNome()
+
+
+
+
+// variavel com const
+
+// const nome = "Luciano"
+
+// function meuNome(){
+// 	const nome = "João"
+// 	console.log(nome)
+// }
+
+// console.log(nome)
+
+// meuNome()
