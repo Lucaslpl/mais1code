4149894304df24c6be909fcb2dbf316e25f131ac
Lucas Luna <lucasbereshit@gmail.com>
2022-08-19 23:00:22 -0300

    manipulando conteúdo

---
+++ js/manipulando-conteudo.js
@@ -0,0 +1,11 @@
+/*
+        Conteúdo relacionado a mudança de texto e de manipulação dos elementos
+*/
+
+// element.innerText - muda o conteúdo do elemento selecionado no HTML (texto interno) - apenas no text
+
+// element.textContent - https://developer.mozilla.org/pt-BR/docs/Web/API/Node/textContent (melhor explicado)
+
+// element.innerHTML = "Olá Devs <strong>Birrl</strong>" - adiciona HTML diretamente no meu elemento, com uso de JS
+
+// element.value - atribui um valor a um "input" (html), de maneira que seja pelo JavaScript
