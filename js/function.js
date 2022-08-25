fcee075d8f602d77ab1a1356876e4efc7fea7273
Lucas Luna <lucasbereshit@gmail.com>
2022-08-25 19:59:57 -0300

    functions

---
+++ js/function.js
@@ -0,0 +1,27 @@
+//bt.addEventListener('click',function(){}
+
+/* function helloWorld(){
+  console.log("Hello World")
+}
+
+helloWorld() */
+
+/* function soma(n1, n2){
+console.log(n1 + n2)}
+
+soma(8,2)
+
+function multiplicacao(n1, n2){
+console.log(n1 * n2)}
+
+multiplicacao(7,2) */
+
+function resultado(nota,nomeDoAluno){
+	if(nota >= 6){
+  console.log(nomeDoAluno + " você passou")
+  } else{
+  console.log(nomeDoALuno + " você reprovou")
+  }
+}
+
+resultado(8, "Lucas")
