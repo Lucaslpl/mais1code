0358387e457586e6b81de8818a78a7aca384e2e4
Lucas Luna <lucasbereshit@gmail.com>
2022-08-26 01:07:26 -0300

    pasta dom criada

--- js/looping.js
+++ js/looping.js
@@ -1,12 +1,12 @@
-// LOOPINGS 
+// LOOPINGS
 // ESTRUTURAS DE REPETIÇÕES
 
-const frutas = ["banana","maça", "pera" ];
+const frutas = ['banana', 'maça', 'pera']
 
 /*  1
 for(var i=0; i<frutas.lenght; i++){
 console.log(frutas[i])
-} */ 
+} */
 
 /* 2
 for(var i of frutas){
